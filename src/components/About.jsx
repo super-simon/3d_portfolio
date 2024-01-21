6e7908774db6c657d4bcc3a49328de6e255f4aa6
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2024-01-21 08:03:05 +0200

    experience

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */
 import { motion } from "framer-motion";
 import { Tilt } from "react-tilt";
 import { services } from "../constants";
